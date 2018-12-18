e49f52f7b367a001556057f6ab05a1a6615d3691
Theodore R. Smith <theodore@phpexperts.pro>
2018-12-18 08:31:08 -0600

    (#5) Upgraded to PHP 7.3.0.

--- build-images.sh
+++ build-images.sh
@@ -3,5 +3,5 @@
 docker build base          --tag="phpexperts/php:7"
 docker build laravel       --tag="phpexperts/php:7-laravel"
 docker build laravel-debug --tag="phpexperts/php:7-laravel-debug"
-docker build web           --tag="phpexperts/web:nginx-php7.2"
-docker build web-debug     --tag="phpexperts/web:nginx-php7.2-debug"
+docker build web           --tag="phpexperts/web:nginx-php7.3"
+docker build web-debug     --tag="phpexperts/web:nginx-php7.3-debug"
