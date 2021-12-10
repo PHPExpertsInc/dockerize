58541b67d71ea898e112995075ebe424a2ca62eb
Theodore R. Smith <theodore@phpexperts.pro>
2021-12-10 08:32:27 -0600

    Added install support for PHP 5.6, 8.0, and 8.1.

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -3,16 +3,17 @@
 #PHP_VERSIONS="7.4"
 #PHP_VERSIONS="5.6 7.0 7.1 7.2 7.3 7.4"
 PHP_VERSIONS="5.6 7.0 7.1 7.2 7.3 7.4 8.0"
-#PHP_VERSIONS="7.0 7.1 7.2 7.3 7.4 8.0"
+#HP_VERSIONS="7.0 7.1 7.2 7.3 7.4 8.0"
 #PHP_VERSIONS="8.0"
 cd images
 
 # Build the base linux image first.
+docker rmi phpexperts/linux:latest
 docker build linux --tag="phpexperts/linux:latest"
 
 for VERSION in ${PHP_VERSIONS}; do
   MAJOR_VERSION=${VERSION%.*}
-  # @TODO: Need to add support for v8.0, too.
+
   docker rm $(docker ps -aq)
   docker rmi --force phpexperts/php:latest 2> /dev/null
   docker rmi --force phpexperts/php:${MAJOR_VERSION}-debug 2> /dev/null
@@ -32,8 +33,7 @@ for VERSION in ${PHP_VERSIONS}; do
 
 done
 
-# docker rmi --force phpexperts/php:8 phpexperts/php:8.0 phpexperts/web:nginx-php8
-# docker build base-php8 --tag="phpexperts/php:8.1"
-# docker build base-php8 --tag="phpexperts/php:8"
+docker rmi --force phpexperts/php:8 phpexperts/php:8.1 phpexperts/web:nginx-php8.1
+docker build base-php8 --tag="phpexperts/php:8.1"
 
-# docker build web-php8 --tag="phpexperts/web:nginx-php8"
+docker build web-php8 --tag="phpexperts/web:nginx-php8.1"
