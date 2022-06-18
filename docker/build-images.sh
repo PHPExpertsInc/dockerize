094dfa9689ac45b667f94695957acf61a9ad4bef
Theodore R. Smith <theodore@phpexperts.pro>
2022-06-18 00:05:57 -0500

    Show all of the output of docker build, not just the last 6 lines.

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 PHP_VERSIONS="5.6 7.0 7.1 7.2 7.3 7.4 8.0 8.1"
+#PHP_VERSIONS="7.0 7.1 7.2 7.3 7.4 8.0 8.1"
 #PHP_VERSIONS="7.4 8.0 8.1"
-#PHP_VERSIONS="8.1"
 cd images
 
 # Build the base linux image first.
 docker rmi phpexperts/linux:latest
-docker build linux --tag="phpexperts/linux:latest" --no-cache
+docker build linux --tag="phpexperts/linux:latest" --no-cache --progress=plain
 
 for VERSION in ${PHP_VERSIONS}; do
   MAJOR_VERSION=${VERSION%.*}
@@ -20,18 +20,18 @@ for VERSION in ${PHP_VERSIONS}; do
   docker rmi --force phpexperts/web:nginx-php${VERSION}-debug 2> /dev/null
   docker rmi --force phpexperts/web:nginx-php${VERSION}-debug 2> /dev/null
 
-  docker build base       --tag="phpexperts/php:latest"                 --build-arg PHP_VERSION=$VERSION
-  docker build base       --tag="phpexperts/php:${MAJOR_VERSION}"       --build-arg PHP_VERSION=$VERSION
-  docker build base       --tag="phpexperts/php:${VERSION}"             --build-arg PHP_VERSION=$VERSION
-  docker build base-debug --tag="phpexperts/php:latest-debug"           --build-arg PHP_VERSION=$VERSION
-  docker build base-debug --tag="phpexperts/php:${VERSION}-debug"       --build-arg PHP_VERSION=$VERSION
-  docker build base-debug --tag="phpexperts/php:${MAJOR_VERSION}-debug" --build-arg PHP_VERSION=$VERSION
-  docker build web        --tag="phpexperts/web:nginx-php${VERSION}"       --build-arg PHP_VERSION=$VERSION
-  docker build web-debug  --tag="phpexperts/web:nginx-php${VERSION}-debug" --build-arg PHP_VERSION=$VERSION
+  docker build base       --tag="phpexperts/php:latest"                    --build-arg PHP_VERSION=$VERSION --progress=plain
+  docker build base       --tag="phpexperts/php:${MAJOR_VERSION}"          --build-arg PHP_VERSION=$VERSION --progress=plain
+  docker build base       --tag="phpexperts/php:${VERSION}"                --build-arg PHP_VERSION=$VERSION --progress=plain
+  docker build base-debug --tag="phpexperts/php:latest-debug"              --build-arg PHP_VERSION=$VERSION --progress=plain
+  docker build base-debug --tag="phpexperts/php:${VERSION}-debug"          --build-arg PHP_VERSION=$VERSION --progress=plain
+  docker build base-debug --tag="phpexperts/php:${MAJOR_VERSION}-debug"    --build-arg PHP_VERSION=$VERSION --progress=plain
+  docker build web        --tag="phpexperts/web:nginx-php${VERSION}"       --build-arg PHP_VERSION=$VERSION --progress=plain
+  docker build web-debug  --tag="phpexperts/web:nginx-php${VERSION}-debug" --build-arg PHP_VERSION=$VERSION --progress=plain
 
   if [ "$MAJOR_VERSION" != "8" ]; then
-    docker build base-ioncube --tag="phpexperts/php:${VERSION}-ioncube"          --build-arg PHP_VERSION=$VERSION
-    docker build web-ioncube  --tag="phpexperts/web:nginx-php${VERSION}-ioncube" --build-arg PHP_VERSION=$VERSION
+    docker build base-ioncube --tag="phpexperts/php:${VERSION}-ioncube"          --build-arg PHP_VERSION=$VERSION --progress=plain
+    docker build web-ioncube  --tag="phpexperts/web:nginx-php${VERSION}-ioncube" --build-arg PHP_VERSION=$VERSION --progress=plain
   fi
 done
 
