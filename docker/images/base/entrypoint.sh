d457ad1e7b7fc4fd0b19286740231ceea2c3aa6c
Theodore R. Smith <theodore@phpexperts.pro>
2023-01-17 07:41:52 -0600

    Now, PHP will be launched from a continuously-running container for much faster runtimes at the expense of about 130 MB per container.

--- docker/images/base/entrypoint.sh
+++ docker/images/base/entrypoint.sh
@@ -1,38 +1,11 @@
 #!/usr/bin/env bash
 
-# Set the uid that php-fpm will run as if one was specified
-if [ "$PHP_FPM_USER_ID" != "" ]; then
-    usermod -u $PHP_FPM_USER_ID www-data
-fi
-PHP_VERSION=$(php -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;')
-
-# Set php.ini options
-for TYPE in cli fpm; do
-    PHP_INI=/etc/php/${PHP_VERSION}/${TYPE}/php.ini
-
-    if [ "$PHP_MEMORY_LIMIT" != "" ]; then
-        sed -i "s!memory_limit =.\+!memory_limit = $PHP_MEMORY_LIMIT!g" "$PHP_INI"
-    fi
-
-    # Update the PHP upload_max_filesize setting if one was specified
-    if [ "$PHP_UPLOAD_MAX_FILESIZE" != "" ]; then
-        sed -i "s!upload_max_filesize = 2M!upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE!g" "$PHP_INI"
-    fi
-
-    # Update the post_max_size setting if one was specified
-    if [ "$PHP_POST_MAX_SIZE" != "" ]; then
-        sed -i "s!post_max_size = 8M!post_max_size = $PHP_POST_MAX_SIZE!g" "$PHP_INI"
-    fi
-done
-
-if [ "$RUN_COMPOSER" == "1" ]; then
-    /usr/local/bin/composer "$@"
-    exit
-fi
-
-if [ -z "$INTERACTIVE" ]; then
-    /usr/bin/php "$@"
+if [ -z "$1" ]; then
+    /usr/bin/php
 else
-    "$@"
+    if [[ "$1" == "php" ]] || [[ "$1" == "composer" ]] || [[ "$1" == "sh" ]] || [[ "$1" == "bash" ]]; then
+        "$@"
+    else
+        /usr/bin/php "$@"
+    fi
 fi
-
