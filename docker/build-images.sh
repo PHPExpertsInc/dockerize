7396a485341d58573ef41090ebd69f1e529167e3
Theodore R. Smith <theodore@phpexperts.pro>
2023-01-20 21:58:20 -0600

    Fixed the problem that prevented the web images from being successfully built.

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -17,7 +17,7 @@ docker tag phpexperts/linux:latest phpexperts/linux:$(date '+%Y-%m-%d')
 for VERSION in ${PHP_VERSIONS}; do
   MAJOR_VERSION=${VERSION%.*}
 
-  docker rm $(docker ps -aq)
+#  docker rm $(docker ps -aq)
   docker rmi --force phpexperts/php:latest 2> /dev/null
   docker rmi --force phpexperts/php:latest-debug 2> /dev/null
   docker rmi --force phpexperts/php:${MAJOR_VERSION}-debug 2> /dev/null
@@ -25,19 +25,21 @@ for VERSION in ${PHP_VERSIONS}; do
   docker rmi --force phpexperts/php:${VERSION} 2> /dev/null
   docker rmi --force phpexperts/php:${VERSION}-debug 2> /dev/null
 
+  docker rmi --force phpexperts/web:nginx-php${VERSION} 2> /dev/null
   docker rmi --force phpexperts/web:nginx-php${VERSION}-debug 2> /dev/null
-  docker rmi --force phpexperts/web:nginx-php${VERSION}-debug 2> /dev/null
+  docker rmi --force phpexperts/web:nginx-php${VERSION}-ioncube 2> /dev/null
 
   docker build base       --tag="phpexperts/php:latest"                    --build-arg PHP_VERSION=$VERSION --no-cache --progress=plain
   docker tag phpexperts/php:latest "phpexperts/php:${MAJOR_VERSION}"
   docker tag phpexperts/php:latest "phpexperts/php:${VERSION}"
 
-  docker build full       --tag="phpexperts/php:${VERSION}-full"           --build-arg PHP_VERSION=$VERSION --no-cache --progress=plain
+#  docker build full       --tag="phpexperts/php:${VERSION}-full"           --build-arg PHP_VERSION=$VERSION --no-cache --progress=plain
 
   docker build base-debug --tag="phpexperts/php:latest-debug"              --build-arg PHP_VERSION=$VERSION --no-cache --progress=plain
   docker tag phpexperts/php:latest "phpexperts/php:${MAJOR_VERSION}-debug"
   docker tag phpexperts/php:latest "phpexperts/php:${VERSION}-debug"
 
+  docker tag "phpexperts/php:${VERSION}" phpexperts/php:latest
   docker build web        --tag="phpexperts/web:nginx-php${VERSION}"       --build-arg PHP_VERSION=$VERSION --no-cache --progress=plain
   docker build web-debug  --tag="phpexperts/web:nginx-php${VERSION}-debug" --build-arg PHP_VERSION=$VERSION --no-cache --progress=plain
 
