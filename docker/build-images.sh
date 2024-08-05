b5a35f4adf5b4467b67b51ed37f519dd1b24bed4
Theodore R. Smith <hopeseekr@gmail.com>
2024-08-05 03:46:54 -0500

    Added support for PHP 8.4 Alpha 4.

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PHP_VERSIONS="5.6 7.0 7.1 7.2 7.3 7.4 8.0 8.1 8.2 8.3"
-#PHP_VERSIONS="8.1 8.3"
+#PHP_VERSIONS="5.6 7.0 7.1 7.2 7.3 7.4 8.0 8.1 8.2 8.3"
+PHP_VERSIONS="8.1 8.2 8.3"
 #PHP_VERSIONS="8.2"
 cd images
 
