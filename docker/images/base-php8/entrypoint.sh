aa66d022c4dd527e6f4c8927f604df512aa6ede9
Theodore R. Smith <theodore@phpexperts.pro>
2024-09-26 07:36:44 -0500

    Fixed the PHP 8.4 entrypoint to use the standard entrypoint.

--- docker/images/base-php8/entrypoint.sh
+++ docker/images/base-php8/entrypoint.sh
@@ -1,28 +1,20 @@
 #!/usr/bin/env bash
+#####################################################################
+#   The Dockerize PHP Project                                       #
+#   https://github.com/PHPExpertsInc/docker-php                     #
+#   License: MIT                                                    #
+#                                                                   #
+#   Copyright © 2024 PHP Experts, Inc. <sales@phpexperts.pro>       #
+#       Author: Theodore R. Smith <theodore@phpexperts.pro>         #
+#      PGP Sig: 4BF826131C3487ACD28F2AD8EB24A91DD6125690            #
+#####################################################################
 
-# Set the uid that php-fpm will run as if one was specified
-if [ "$PHP_FPM_USER_ID" != "" ]; then
-    usermod -u $PHP_FPM_USER_ID www-data
+if [ -z "$1" ]; then
+    /usr/bin/php
+else
+    if [[ "$1" == "php" ]] || [[ "$1" == "composer" ]] || [[ "$1" == "sh" ]] || [[ "$1" == "bash" ]]; then
+        $@
+    else
+        /usr/bin/php "$@"
+    fi
 fi
-PHP_VERSION=$(php -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;')
-
-# Set php.ini options
-for TYPE in cli fpm; do
-    PHP_INI=/etc/php/${PHP_VERSION}/${TYPE}/php.ini
-
-    if [ "$PHP_MEMORY_LIMIT" != "" ]; then
-        sed -i "s!memory_limit =.\+!memory_limit = $PHP_MEMORY_LIMIT!g" "$PHP_INI"
-    fi
-
-    # Update the PHP upload_max_filesize setting if one was specified
-    if [ "$PHP_UPLOAD_MAX_FILESIZE" != "" ]; then
-        sed -i "s!upload_max_filesize = 2M!upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE!g" "$PHP_INI"
-    fi
-
-    # Update the post_max_size setting if one was specified
-    if [ "$PHP_POST_MAX_SIZE" != "" ]; then
-        sed -i "s!post_max_size = 8M!post_max_size = $PHP_POST_MAX_SIZE!g" "$PHP_INI"
-    fi
-done
-
-/usr/bin/php "$@"
