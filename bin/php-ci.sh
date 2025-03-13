d3bd78f34ac607e413244a994b8bcb00035dbcc2
Theodore R. Smith <hopeseekr@gmail.com>
2025-03-13 17:25:06 -0500

    [php-ci] Refactored a lot.

--- bin/php-ci.sh
+++ bin/php-ci.sh
@@ -16,19 +16,29 @@ if [ -f .env ]; then
     source .env
 fi
 
+ORIG_PHPUNIT_V=''
+if [ -z "$PHPUNIT_V" ]; then
+    ORIG_PHPUNIT_V="$PHPUNIT_V"
+fi
+
 time for PHPV in ${SUPPORTED_PHP_VERSIONS}-debug; do
     PHP_VERSION=$PHPV composer --version
     PHP_VERSION=$PHPV composer update
+    DISPLAY_WARNINGS="--display-warnings --display-deprecations"
     # Assign the PHPUnit version, if it's not set inside of .env or $_ENV.
-    if [ -z "$PHPUNIT_V" ]; then
+    if [ "$ORIG_PHPUNIT_V" == '' ]; then
         if [ $PHPV == '7.0' ]; then
             PHPUNIT_V='6'
+            DISPLAY_WARNINGS=''
         elif [ $PHPV == '7.1' ]; then
             PHPUNIT_V='7'
+            DISPLAY_WARNINGS=''
         elif [ $PHPV == '7.2' ]; then
             PHPUNIT_V='8'
+            DISPLAY_WARNINGS=''
         elif [ $PHPV == '7.3' ] || [ $PHPV == '7.4' ] || [ $PHPV == '8.0' ]; then
             PHPUNIT_V='9'
+            DISPLAY_WARNINGS=''
         elif [ $PHPV == '8.1' ]; then
             PHPUNIT_V='10'
         elif [ $PHPV == '8.2' ]; then
@@ -39,12 +49,13 @@ time for PHPV in ${SUPPORTED_PHP_VERSIONS}-debug; do
     fi
 
     if [ -f phpunit.v${PHPUNIT_V}.xml ]; then
-        PHP_VERSION=$PHPV phpunit -c phpunit.v${PHPUNIT_V}.xml
+        echo PHP_VERSION=$PHPV phpunit -c phpunit.v${PHPUNIT_V}.xml "$DISPLAY_WARNINGS"
+        PHP_VERSION=$PHPV phpunit -c phpunit.v${PHPUNIT_V}.xml "$DISPLAY_WARNINGS"
     else
-        PHP_VERSION=$PHPV phpunit
+        PHP_VERSION=$PHPV phpunit "$DISPLAY_WARNINGS"
     fi
 
-    echo "Tested: PHP v$PHPV"
+    echo "Tested: PHP v$PHPV via PHPUnit v$PHPUNIT_V"
     sleep 2
 done
 
