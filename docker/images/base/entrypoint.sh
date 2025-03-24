e0b6819a135f3d806f741b01a2527e21c3fb258c
Theodore R. Smith <theodore@phpexperts.pro>
2025-03-24 13:34:41 -0500

    [fixed] Ensured proper command execution in the entrypoint.

--- docker/images/base/entrypoint.sh
+++ docker/images/base/entrypoint.sh
@@ -13,7 +13,7 @@ if [ -z "$1" ]; then
     /usr/bin/php
 else
     if [[ "$1" == "php" ]] || [[ "$1" == "composer" ]] || [[ "$1" == "sh" ]] || [[ "$1" == "bash" ]]; then
-        $@
+        "$@"
     else
         /usr/bin/php "$@"
     fi
