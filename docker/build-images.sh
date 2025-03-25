e07c4d22c031a7557a4bcb0bded9510446750ebf
Theodore R. Smith <hopeseekr@gmail.com>
2025-03-25 13:21:46 -0500

    Added the PHP Extension Builder system.

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -4,14 +4,14 @@
 #   https://github.com/PHPExpertsInc/docker-php                     #
 #   License: MIT                                                    #
 #                                                                   #
-#   Copyright © 2024 PHP Experts, Inc. <sales@phpexperts.pro>       #
+#   Copyright © 2020-2025 PHP Experts, Inc. <sales@phpexperts.pro>  #
 #       Author: Theodore R. Smith <theodore@phpexperts.pro>         #
 #      PGP Sig: 4BF826131C3487ACD28F2AD8EB24A91DD6125690            #
 #####################################################################
 
 PHP_VERSIONS="5.6 7.0 7.1 7.2 7.3 7.4 8.0 8.1 8.2 8.3 8.4"
 #PHP_VERSIONS="7.4 8.0 8.1 8.2 8.3 8.4"
-#PHP_VERSIONS="8.3"
+#PHP_VERSIONS="8.4"
 cd images
 
 # Create the apt cache volume
@@ -43,13 +43,6 @@ if [ ! -f ./base-oracle/.build-assets/instantclient-sdk-linux.x64-21.12.0.0.0dbr
     mv instantclient-sdk-linux.x64-21.12.0.0.0dbru.zip ./base-oracle/.build-assets/
 fi
 
-# @TODO: Investigate whether it's really best to download all of these extensions in ./build.images.
-if [ ! -f ./base-full/.build-assets/uuid-1.2.1.tar.gz ]; then
-    curl -fsSLO https://pecl.php.net/get/uuid-1.2.1.tgz
-    mkdir -p ./base-full/.build-assets
-    mv uuid-1.2.1.tar.gz ./base-full/.build-assets/uuid-1.2.1.tar.gz
-fi
-
 for VERSION in ${PHP_VERSIONS}; do
   MAJOR_VERSION=${VERSION%.*}
 
@@ -67,6 +60,8 @@ for VERSION in ${PHP_VERSIONS}; do
   docker build base       --tag="phpexperts/php:latest"                    --build-arg VOLUME="apt-cache:/var/lib/apt" --build-arg PHP_VERSION=$VERSION --no-cache --progress=plain
   docker tag phpexperts/php:latest "phpexperts/php:${MAJOR_VERSION}"
   docker tag phpexperts/php:latest "phpexperts/php:${VERSION}"
+
+  docker build ext-builder --tag="phpexperts/ext-builder:${VERSION}" --build-arg VOLUME="apt-cache:/var/lib/apt" --build-arg PHP_VERSION=$VERSION --no-cache --progress=plain
 
   docker rmi --force phpexperts/php:${VERSION}-full
   docker build base-full  --tag="phpexperts/php:${VERSION}-full"           --build-arg VOLUME="apt-cache:/var/lib/apt" --build-arg PHP_VERSION=$VERSION --no-cache --progress=plain
@@ -95,6 +90,7 @@ done
 
 docker volume rm apt-cache
 
+# PHP-Next builds...
 #docker rmi --force phpexperts/php:8.2 phpexperts/web:nginx-php8.2
 #docker build base-php8 --tag="phpexperts/php:8.2" --no-cache --progress=plain
 
