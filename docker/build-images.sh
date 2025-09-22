48ce1f2cc52075447cde2502cfd45f1c3c9c8b65
Theodore R. Smith <hopeseekr@gmail.com>
2025-09-21 22:35:18 -0500

    Embedded the default nginx virtualhost directly into the image.

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -74,9 +74,14 @@ for VERSION in ${PHP_VERSIONS}; do
   docker tag phpexperts/php:latest-debug "phpexperts/php:${VERSION}-debug"
 
   # docker tag "phpexperts/php:${VERSION}" phpexperts/php:latest
+
+  mkdir -p web/.build-assets
+  cp ../web/sites/001_default.conf web/.build-assets
+
   docker build web        --tag="phpexperts/web:nginx-php${VERSION}"       --build-arg VOLUME="apt-cache:/var/lib/apt" --build-arg PHP_VERSION=$VERSION --no-cache --progress=plain
   docker build web-debug  --tag="phpexperts/web:nginx-php${VERSION}-debug" --build-arg VOLUME="apt-cache:/var/lib/apt" --build-arg PHP_VERSION=$VERSION --no-cache --progress=plain
 
+  rm -r web/.build-assets
 
   # IonCube doesn't support PHP v8.0 or v8.3.
   if [[ "$VERSION" != "8.0" && "$VERSION" != "8.3" && "$VERSION" != "8.4" ]]; then
